4fa1fa095190d810df60de747088753749ea79dd
John Kane <john.kane@spartan-solutions.com>
2013-07-27 11:50:27 +0100

    Pull out the md5 hash for each file during analysis.

--- src/leiningen/s3_sync.clj
+++ src/leiningen/s3_sync.clj
@@ -1,6 +1,35 @@
-(ns leiningen.s3-sync)
+(ns leiningen.s3-sync
+  (:require [pandect.core :as p]))
 
-(defn s3-sync
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+;; Get the list of local files
+;; For each
+;;   check whether the file exists on s3
+;;   if not then upload
+;;   if it does check whether the md5 hashes match
+;;     if not then upload  
+
+(declare relative-path)
+(declare path->file-details)
+
+(defn analyse-dir
+  "Analyse a local directory returnings a set
+   of file details describing the relative path
+   and md5 checksum of all the files (recursively)
+   under the directory."
+  [dir-path]
+  (let [root-dir (clojure.java.io/file dir-path)
+        abs-root-dir-path (.getAbsolutePath root-dir)]
+    (->> (file-seq root-dir)
+         (filter #(not (.isDirectory %)))
+         (map (partial path->file-details abs-root-dir-path))
+         (set))))
+ 
+(defn- relative-path [root target]
+  (.replaceAll target (str "^" root "/") ""))
+
+(defn- path->file-details [root-path file]
+  (let [absolute-path (.getAbsolutePath file)
+        rel-path (relative-path root-path absolute-path)
+        md5 (p/md5-file absolute-path)]
+    {:path rel-path :md5 md5}))
+ 
--- test/leiningen/s3_sync_test.clj
+++ test/leiningen/s3_sync_test.clj
@@ -1,35 +1,14 @@
 (ns leiningen.s3-sync-test
-  (require [clojure.test :refer :all]))
-
-;; Get the list of local files
-;; For each
-;;   check whether the file exists on s3
-;;   if not then upload
-;;   if it does check whether the md5 hashes match
-;;     if not then upload  
-
-(defn relative-path [root target]
-  (.replaceAll target (str "^" root "/") ""))
-
-(defn analyse [dir]
-  (let [root-dir (clojure.java.io/file dir)
-        root-path (.getAbsolutePath root-dir)]
-    (->> dir
-        (clojure.java.io/file)
-        (file-seq)
-        (filter #(not (.isDirectory %)))
-        (map #(.getAbsolutePath %))
-        (map (partial relative-path root-path))
-        (map (partial hash-map :path))
-        (set))))
+  (:require [clojure.test :refer :all]
+            [leiningen.s3-sync :as sync]))
 
 (def expected-dir-details
-  #{{:path "hello.txt"} 
-    {:path "world.txt"} 
-    {:path "sub/continent.txt"}})
+  #{{:path "hello.txt" :md5 "09f7e02f1290be211da707a266f153b3"} 
+    {:path "world.txt" :md5 "52f83ff6877e42f613bcd2444c22528c"} 
+    {:path "sub/continent.txt" :md5 "4e3a9a3106fb00b721dc8c2cfac98351"}})
 
 (deftest resolving-the-md5-of-each-file-in-a-dir
-  (let [local-dir-details (analyse "test/example")]
+  (let [local-dir-details (sync/analyse-dir "test/example")]
     (is (= 3 (count local-dir-details)))
     (is (= expected-dir-details local-dir-details))))
 
