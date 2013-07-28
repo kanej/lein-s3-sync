282329fa8a1fe33285b988b43518512ea5631416
John Kane <john.kane@spartan-solutions.com>
2013-07-28 14:08:02 +0100

    Update project description.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject lein-s3-sync "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sync local folders to s3"
+  :url "http://github.com/kanej/lein-s3-sync"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-aws-s3 "0.3.6"]
                  [pandect "0.3.0"]]
-  :eval-in-leiningen true
-  )
+  :eval-in-leiningen true)
