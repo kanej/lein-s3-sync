bdd6725764d90c4904b4e9dcad92b58a1e2295e2
John Kane <john.kane@spartan-solutions.com>
2013-11-18 23:24:45 +0000

    Cleanup of user.clj

--- s3-sync/dev/user.clj
+++ s3-sync/dev/user.clj
@@ -9,8 +9,7 @@
             [me.kanej.s3-sync.s3 :as s3]
             [me.kanej.s3-sync.merge :as m]
             [aws.sdk.s3 :as aws]
-            [me.kanej.s3-sync :as s3s]
-            ))
+            [me.kanej.s3-sync :as s3s]))
 
 (def cred
   (when (.exists (clojure.java.io/file "profiles.clj"))
@@ -23,12 +22,3 @@
 (def local-dir "test/example")
 
 (def options (merge cred {:local-dir local-dir :bucket-name bucket-name}))
-
-;;(def sync-state (s3s/analyse-sync-state options))
-
-
-;;(s3/analyse-s3-bucket cred bucket-name ["world.txt"])
-
-
-;;(s3/make-file-public aws-credentials "s3-sync-test" "world.txt")
-;;(aws/update-object-acl aws-credentials "s3-sync-test" "world.txt" (aws/grant :all-users :read))
