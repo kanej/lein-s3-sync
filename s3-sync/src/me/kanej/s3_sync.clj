068750b90f9b0138832ecf3d2392e4634871ff21
John Kane <john.kane@spartan-solutions.com>
2013-11-18 23:14:19 +0000

    Reorder functions for clarity.

--- s3-sync/src/me/kanej/s3_sync.clj
+++ s3-sync/src/me/kanej/s3_sync.clj
@@ -3,36 +3,9 @@
             [me.kanej.s3-sync.s3 :as s3]
             [me.kanej.s3-sync.merge :as m]))
 
-(defn capture-file-details [{:keys [access-key secret-key local-dir bucket-name] :as sync-state}]
-  (let [cred (select-keys sync-state [:access-key :secret-key])
-        local-file-details (fs/analyse-local-directory local-dir)
-        file-paths (->> local-file-details
-                       (:local-file-details)
-                       (map :path))
-        remote-file-details (s3/analyse-s3-bucket cred bucket-name file-paths)]
-    (merge sync-state {:local-file-details local-file-details
-                       :remote-file-details remote-file-details})))
-
-(defn calculate-deltas [{:keys [errors local-file-details remote-file-details] :as sync-state}]
-  (if (empty? errors)
-    (let [deltas (m/generate-deltas local-file-details remote-file-details)]
-      (assoc sync-state :deltas deltas))))
-
-(defn push-deltas-to-s3 [{:keys [errors local-dir bucket-name] :as sync-state}]
-  (when (empty? errors)
-    (let [cred (select-keys sync-state [:access-key :secret-key])]
-      (loop [deltas (:deltas sync-state)]
-        (if (not (empty? deltas))
-          (let [[op {rel-path :path}] (first deltas)]
-            (print "  " rel-path "uploading ...")
-            (s3/put-file
-             cred
-             bucket-name
-             rel-path
-             (fs/combine-path local-dir rel-path))
-            (println "\r  " rel-path "done." (apply str (take 30 (repeat " "))))
-            (recur (rest deltas)))))))
-  sync-state)
+(declare capture-file-details)
+(declare calculate-deltas)
+(declare push-deltas-to-s3)
 
 (declare print-delta-summary)
 (declare print-sync-complete-message)
@@ -50,6 +23,41 @@
         (print-delta-summary)
         (push-deltas-to-s3)
         (print-sync-complete-message))))
+
+;; Private Logic functions
+
+(def padding (apply str (take 30 (repeat " "))))
+
+(defn- capture-file-details [{:keys [access-key secret-key local-dir bucket-name] :as sync-state}]
+  (let [cred (select-keys sync-state [:access-key :secret-key])
+        local-file-details (fs/analyse-local-directory local-dir)
+        file-paths (->> local-file-details
+                       (:local-file-details)
+                       (map :path))
+        remote-file-details (s3/analyse-s3-bucket cred bucket-name file-paths)]
+    (merge sync-state {:local-file-details local-file-details
+                       :remote-file-details remote-file-details})))
+
+(defn- calculate-deltas [{:keys [errors local-file-details remote-file-details] :as sync-state}]
+  (if (empty? errors)
+    (let [deltas (m/generate-deltas local-file-details remote-file-details)]
+      (assoc sync-state :deltas deltas))))
+
+(defn- push-deltas-to-s3 [{:keys [errors local-dir bucket-name] :as sync-state}]
+  (when (empty? errors)
+    (let [cred (select-keys sync-state [:access-key :secret-key])]
+      (loop [deltas (:deltas sync-state)]
+        (if (not (empty? deltas))
+          (let [[op {rel-path :path}] (first deltas)]
+            (print "  " rel-path "uploading ...")
+            (s3/put-file
+             cred
+             bucket-name
+             rel-path
+             (fs/combine-path local-dir rel-path))
+            (println "\r  " rel-path "done." padding)
+            (recur (rest deltas)))))))
+  sync-state)
 
 ;; Print Functions
 
