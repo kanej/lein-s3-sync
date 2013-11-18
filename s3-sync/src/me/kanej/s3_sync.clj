dd866ba249538971ff2058ea6b8ca3c5a14cc59a
John Kane <john.kane@spartan-solutions.com>
2013-11-18 21:49:38 +0000

    Destructuring to remove some lets.

--- s3-sync/src/me/kanej/s3_sync.clj
+++ s3-sync/src/me/kanej/s3_sync.clj
@@ -3,7 +3,7 @@
             [me.kanej.s3-sync.s3 :as s3]
             [me.kanej.s3-sync.merge :as m]))
 
-(def padding "                                           ")
+(def padding (apply str (take 30 (repeat " "))))
 
 (defn analyse-sync-state [cred dir-path bucket-name]
   (let [local-file-state (fs/analyse-local-directory dir-path)
@@ -13,13 +13,10 @@
         s3-file-state (s3/analyse-s3-bucket cred bucket-name file-paths)]
     (merge local-file-state s3-file-state)))
 
-(defn calculate-deltas-from [{:keys [errors] :as sync-state}]
+(defn calculate-deltas-from [{:keys [errors local-file-details remote-file-details] :as sync-state}]
   (if (empty? errors)
-    (let [local-file-details (:local-file-details sync-state)
-          s3-file-details (:remote-file-details sync-state)
-          deltas (m/generate-deltas local-file-details s3-file-details)]
-      (assoc sync-state :deltas deltas))
-    sync-state))
+    (let [deltas (m/generate-deltas local-file-details remote-file-details)]
+      (assoc sync-state :deltas deltas))))
 
 (defn resolve-full-path [root-path rel-path]
   (let [root (clojure.java.io/file root-path)
@@ -27,21 +24,19 @@
         abs-path (.getAbsolutePath combined)]
     abs-path))
 
-(defn push-changes-to-s3 [cred {:keys [errors] :as sync-state}]
+(defn push-changes-to-s3 [cred {:keys [errors root-dir-path bucket-name] :as sync-state}]
   (when (empty? errors)
-    (let [local-root-path (:root-dir-path sync-state)
-          bucket-name (:bucket-name sync-state)]
-      (loop [deltas (:deltas sync-state)]
-        (if (not (empty? deltas))
-          (let [[op {rel-path :path}] (first deltas)]
-            (print "  " rel-path "uploading ...")
-            (s3/put-file
-              cred
-              bucket-name
-              rel-path
-              (resolve-full-path local-root-path rel-path))
-            (println "\r  " rel-path "done." padding)
-            (recur (rest deltas)))))))
+    (loop [deltas (:deltas sync-state)]
+      (if (not (empty? deltas))
+        (let [[op {rel-path :path}] (first deltas)]
+          (print "  " rel-path "uploading ...")
+          (s3/put-file
+            cred
+            bucket-name
+            rel-path
+            (resolve-full-path root-dir-path rel-path))
+          (println "\r  " rel-path "done." padding)
+          (recur (rest deltas))))))
   sync-state)
 
 (defn- print-sync-state [{:keys [errors deltas] :as sync-state}]
